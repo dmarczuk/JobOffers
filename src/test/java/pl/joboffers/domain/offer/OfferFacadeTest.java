1319a402aa6ce92b5414b483637c340aadb21179
Dominik <marczukd@gmail.com>
2023-11-21 13:06:55 +0100

    added signature missing tests from course

--- src/test/java/pl/joboffers/domain/offer/OfferFacadeTest.java
+++ src/test/java/pl/joboffers/domain/offer/OfferFacadeTest.java
@@ -114,4 +114,34 @@ class OfferFacadeTest {
         assertThat(allOffers.contains(new Offer(16, "urlName3"))).isFalse();
     }
 
+    @Test
+    public void should_save_4_offers_when_there_are_no_offers_in_database() {
+
+    }
+
+    @Test
+    public void should_save_only_2_offers_when_repository_had_4_added_with_offer_urls() {
+
+    }
+
+    @Test
+    public void should_throw_duplicate_key_exception_when_with_offer_url_exists() {
+
+    }
+
+    @Test
+    public void should_throw_not_found_exception_when_offer_not_found() {
+
+    }
+
+    @Test
+    public void should_fetch_from_jobs_from_remote_and_save_all_offers_when_repository_is_empty() {
+
+    }
+
+    @Test
+    public void should_find_offer_by_id_when_offer_was_saved() {
+
+    }
+
 }
