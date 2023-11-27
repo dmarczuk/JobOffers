7a15bbc0685fd60df071fe4a9f2324324570a165
Dominik <marczukd@gmail.com>
2023-11-27 18:49:40 +0100

    add BaseIntegrationTest with dependencies

---
+++ src/integration/java/pl/joboffers/feature/TestWithoutName.java
@@ -0,0 +1,12 @@
+package pl.joboffers.feature;
+
+import org.junit.jupiter.api.Test;
+import pl.joboffers.BaseIntegrationTest;
+
+public class TestWithoutName extends BaseIntegrationTest {
+
+    @Test
+    public void f() {
+
+    }
+}
