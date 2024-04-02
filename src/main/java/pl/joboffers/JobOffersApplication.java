840ff50db2f4e2a5c9312129c7f5bc3f0212b377
Dominik <marczukd@gmail.com>
2024-04-02 15:03:25 +0200

    change application properties for deploying aws

--- src/main/java/pl/joboffers/JobOffersApplication.java
+++ src/main/java/pl/joboffers/JobOffersApplication.java
@@ -3,13 +3,11 @@ package pl.joboffers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import pl.joboffers.infrastructure.offer.http.OfferFetcherRestTemplateConfigurationProperties;
 import pl.joboffers.infrastructure.security.jwt.JwtConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({OfferFetcherRestTemplateConfigurationProperties.class, JwtConfigurationProperties.class})
-@EnableMongoRepositories
 public class JobOffersApplication {
 
     public static void main(String[] args) {
