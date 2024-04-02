5f084e972cc9f9a363588ea55320eec328f98e70
Dominik <marczukd@gmail.com>
2024-04-02 08:44:34 +0200

    fix wrong DynamicPropertySource in Integration Test

--- src/main/java/pl/joboffers/JobOffersApplication.java
+++ src/main/java/pl/joboffers/JobOffersApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import pl.joboffers.infrastructure.offer.http.OfferFetcherRestTemplateConfigurationProperties;
 import pl.joboffers.infrastructure.security.jwt.JwtConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({OfferFetcherRestTemplateConfigurationProperties.class, JwtConfigurationProperties.class})
-@EnableScheduling
 @EnableMongoRepositories
 public class JobOffersApplication {
 
